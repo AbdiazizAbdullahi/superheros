49f5cc92741e5d1463d9723e78d2f4a1978ac076
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-03-27 18:22:48 +0300

    done

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -2,13 +2,15 @@ class HerosController < ApplicationController
 
     def index
         heroes = Hero.all 
-        render json: heroes, each_serializer: HeroSerializer 
+        render json: heroes 
+        # each_serializer: HeroSerializer 
     end
 
     def show
         hero = Hero.find_by(id: params[:id])
         if hero 
-            render json: hero, serializer: HeroSerializer
+            render json: hero, except: [:created_at, :updated_at]
+            # , serializer: HeroSerializer
         else 
             render json: { error: "Hero not found"}, status: :not_found
         end
